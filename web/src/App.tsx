e77c534f558564cbc10c8dc22e5abb90bab92591
ThiagoMaurat <thiagomaurat@hotmail.com>
2023-01-25 19:24:38 -0300

    feat:Header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,11 @@
-import { Habit } from "./components/Habit";
 import "./styles/global.css";
+import logoImage from "./assets/logoImage.svg";
+import { Header } from "./components/Header";
 
-function App() {
-  return <Habit />;
+export function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <Header />
+    </div>
+  );
 }
-
-export default App;
