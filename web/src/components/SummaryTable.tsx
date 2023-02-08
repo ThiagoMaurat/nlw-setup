9cbaa32a0d7dc10acd3b8b2d0b028cf4cd3a1c65
ThiagoMaurat <thiagomaurat@hotmail.com>
2023-02-08 20:11:52 -0300

    feat:ServerQuery

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 import { HabitDay } from "./HabitDay";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 
+const summaryDates = generateDatesFromYearBeginning();
+
+const minimumSummaryDatesSize = 18 * 7; // 18 weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
+
+console.log(summaryDates);
 export const SummaryTable = () => {
   return (
     <div className="w-full flex">
@@ -11,7 +18,7 @@ export const SummaryTable = () => {
           return (
             <div
               key={`weekDay${index}`}
-              className="text-zinc-400 font-bold text-xl h-10 w-10 flex items-center justify-center"
+              className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
             >
               {days}
             </div>
@@ -20,7 +27,19 @@ export const SummaryTable = () => {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        <HabitDay />
+        {summaryDates.map((date) => {
+          return <HabitDay key={date.toString()} />;
+        })}
+
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+            return (
+              <div
+                key={i}
+                className="w-10 h-10 bg-zinc-900 text-white border-2 border-zinc-800 m-2 flex items-center justify-center rounded opacity-40 cursor-not-allowed"
+              />
+            );
+          })}
       </div>
     </div>
   );
